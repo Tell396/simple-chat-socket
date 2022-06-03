87e79bf84c86283c46bff06ecd5923802620061d
Tell396 <vova21473@gmail.com>
2022-06-03 21:35:04 +1000

    Story #2. Rooms were added.

--- client/script.js
+++ client/script.js
@@ -1,9 +1,17 @@
 import { io } from 'socket.io-client'
 
-const socket = io('http://localhost:3000') // Creating  new socket. Now server can connect to port 8080
+const socket = io('http://localhost:3000') // Creating  new socket. Now server can connect to port 3000, client -- 8080
 socket.on('connect', () => {
     displayMessage(`You connected with id: ${socket.id}!`) // Now we can setup the connection with a client and server!
 })
+socket.on('receive-message', message => {
+    displayMessage(message)
+})
+
+/* Choose the name of event what do you want;
+    As second argument you can choose what you want --
+    -- for me it's a: 10, 'Hi', {a: 'a'} */
+//socket.emit('custom-event', 10, 'Hi', {a: 'a'})// For listen events coming down from the server.
 
 const joinRoomButton = document.getElementById('room-button')
 const messageInput = document.getElementById('message-input')
@@ -20,13 +28,17 @@ form.addEventListener('submit', e => {
     e.preventDefault()
     const message = messageInput.value
     const room = roomInput.value
-
+    
     if (message === "") return
     displayMessage(message)
-
+    socket.emit('send-message', message, room)
+    
     messageInput.value = ""
 })
 
 joinRoomButton.addEventListener('click', () => {
     const room = roomInput.value
+    socket.emit('join-room', room, message => {
+        displayMessage(message)
+    })
 })
--- server/server.js
+++ server/server.js
@@ -1,9 +1,30 @@
-const io = require('socket.io')(3000, {
-    cors: {
-        origin: ['http://localhost:8080'],
-    },
-}) // Declare a variable using socket.io and port 3000.
+const { instrument } = require("@socket.io/admin-ui");
 
-io.on('connection', socket => {
-    console.log(socket.id)
-})
+const io = require("socket.io")(3000, {
+  cors: {
+    origin: ["http://localhost:8080", "https://admin.socket.io"],
+  },
+}); // Declare a variable using socket.io and port 3000.
+
+io.on("connection", socket => {
+  //console.log(socket.id)
+  /* socket.on("custom-event", (number, string, obj) => { // Listen and hear parameters from the client
+        console.log(number, string, obj)
+    }) */
+
+  socket.on("send-message", (message, room) => {
+    if (room === "") {
+      socket.broadcast.emit("receive-message", message); // Receive message to other client. // Use socket.broadcast for send to every other socket isn"t me.
+    } else {
+      socket.to(room).emit("receive-message", message);
+    }
+  });
+
+  socket.on("join-room", (room, cb) => {
+    // We say the server, what we join to the room
+    socket.join(room);
+    cb(`Joined to ${room}`);
+  });
+});
+
+instrument(io, { auth: false });
